b6277e826838339d9f5b9cfb6d9aa8e3d2cf65a0
majhaj <maja.hajda@gmail.com>
2023-08-30 15:58:20 +0200

    first commit

---
+++ API/Data/DbContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {   
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+        
+    }
+}
