4cd613bc688eb2ec56fa3f7cbb0b41e357708f87
majhaj <maja.hajda@gmail.com>
2023-09-01 16:34:03 +0200

    Adding gitignore

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public UsersController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dbContext.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
